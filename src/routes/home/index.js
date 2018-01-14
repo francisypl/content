172fd7e825d699932ad478970bef7501857693d0
Francis Yuen <fyuen@godaddy.com>
2018-01-13 16:59:16 -0800

    file uploader

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -9,12 +9,12 @@
 
 import React from 'react';
 import Home from './Home';
-import fetch from '../../core/fetch';
+// import fetch from '../../core/fetch';
 
 export const path = '/';
 export const action = async (state) => {
-  const response = await fetch('/graphql?query={news{title,link,contentSnippet}}');
-  const { data } = await response.json();
+  // const response = await fetch('/graphql?query={news{title,link,contentSnippet}}');
+  // const { data } = await response.json();
   state.context.onSetTitle('React.js Starter Kit');
-  return <Home news={data.news} />;
+  return <Home />;
 };
