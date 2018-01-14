2abdf6d835beb1496535e2cc12b3e2ca6f810e7d
Francis Yuen <fyuen@godaddy.com>
2018-01-14 11:17:55 -0800

    view page is working visually

--- src/components/ContentView/ContentView.js
+++ src/components/ContentView/ContentView.js
@@ -2,6 +2,7 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ContentView.scss';
+import Link from '../Link';
 
 class ContentView extends Component {
 
@@ -12,10 +13,17 @@ class ContentView extends Component {
   };
 
   render() {
+    const { price, id } = this.props;
     return (
       <div className={s.root}>
         <div className={s.container}>
-          {JSON.stringify(this.props)}
+          <div className={s.textbox}>
+            <div className={s.price}>{`${price} wei`}</div>
+            <button className={s.cButton}>{'Pay To View Content'}</button>
+            <Link className={s.cButton} to={`/promote/${id}`}>
+              <span className={s.btnText}>{'Promote'}</span>
+            </Link>
+          </div>
         </div>
       </div>
     );
